1f90fef825dc3c7ec0db6388e02e69ac370b9cf3
BryanWV <wilchesbryan@gmail.com>
2024-08-13 19:36:16 -0500

    Modifying Fibonacci counter code, adding images folder

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4de0fd9fef3d76cd7790de2a6a0aee6e2f43201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff4b9dcc9b3bd5712883208b81308e17aed8303")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
